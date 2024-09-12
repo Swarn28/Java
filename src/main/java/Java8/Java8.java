99672a190b982bf4791158023b22ef76a99dfdda
Swarn28 <swarnbedi28@gmail.com>
2024-09-12 08:41:18 +0530

    Adding new data...

--- src/main/java/Java8/Java8.java
+++ src/main/java/Java8/Java8.java
@@ -1,7 +1,9 @@
 package Java8;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Java8 {
 
@@ -28,7 +30,7 @@ public class Java8 {
         outputList.forEach(System.out::println);
 
         //Sum of List of Integers Using Streams **
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5,5,4,2);
         int sum = numbers.stream().mapToInt(Integer::intValue).sum();
         System.out.println("Sum is : " +sum);
 
@@ -36,10 +38,20 @@ public class Java8 {
         Optional<Integer> max = numbers.stream().max(Integer::compare);
         System.out.println("Max is: " +max.get());
 
+        // second way to find max
+        numbers.stream().mapToInt(Integer::intValue).max();
+
+        //way to find average
+        numbers.stream().mapToInt(Integer::intValue).average();
+
         // Sort List<String>
         List<String> sorted = list.stream().sorted().collect(Collectors.toList());
         System.out.println("Sorted list is");
         sorted.forEach(System.out::println);
+
+        // Sort a List of Strings by Length.
+        List<String> sortedByLength = list.stream().sorted(
+                Comparator.comparingInt(String::length)).collect(Collectors.toList());
 
         // Reverse Sorted List<String>
         List<String> reverseSorted = list.stream().sorted(Comparator.reverseOrder()).
@@ -52,6 +64,48 @@ public class Java8 {
                                         collect(Collectors.toList());
         System.out.println("Reverse integer list..");
         reverseInteger.forEach(System.out::println);
+
+
+        // create a hashmap of Map<String, Integer> where key is the String and Integer is its length.
+        Map<String,Integer> lengthMap = list.stream().collect(Collectors.toMap(s->s,s->s.length()));
+        System.out.println("map with length is: ");
+        lengthMap.forEach((k,v) -> {
+            System.out.println(k + "," + v);
+        });
+
+        // Count and group by.
+        List<String> fruitList = Arrays.asList("apple","banana","apple","pears","orange","banana");
+        Map<String, Long> countMap = fruitList.stream().collect(Collectors.groupingBy(
+                Function.identity(), Collectors.counting()));
+        System.out.println("Grouped by count map is: ");
+        countMap.forEach((k,v)->{
+            System.out.println(k + ", " + v);
+        });
+
+        // FInd Duplicates
+        Set<String> duplicates = fruitList.stream().filter(n->Collections.frequency(fruitList,n)>1).
+                collect(Collectors.toSet());
+        System.out.println("Duplicates are : ");
+        duplicates.forEach(System.out::println);
+
+        // Convert a list into list of length
+        List<Integer> lengthList = fruitList.stream().map(s->s.length()).collect(Collectors.toList());
+        System.out.println("Length list is: ");
+        lengthList.forEach(System.out::println);
+
+        // Remove duplicates from a list
+        List<Integer> collect = numbers.stream().distinct().collect(Collectors.toList());
+
+
+        //partioning by
+        Map<Boolean, List<Integer>> isEvenMap = numbers.stream().collect(Collectors.partitioningBy(
+                num -> num%2==0
+        ));
+
+        System.out.println("Partionied map is: ");
+        isEvenMap.forEach((k,v) -> {
+            System.out.println(k + ", " +v);
+        });
 
     }
 }
