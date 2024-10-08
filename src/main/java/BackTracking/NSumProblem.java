c7cbf5a6d551ccf27f62f8e7f8e6c446766287d7
Swarn28 <swarnbedi28@gmail.com>
2024-10-08 08:57:51 +0530

    Adding back

--- src/main/java/BackTracking/NSumProblem.java
+++ src/main/java/BackTracking/NSumProblem.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-* https://www.youtube.com/watch?v=yFfv03AE_vA
-* https://leetcode.com/problems/combination-sum/description/
-* https://www.geeksforgeeks.org/ways-to-sum-to-n-using-natural-numbers-up-to-k-with-repetitions-allowed/
-* Medium
-*
-* if given Sum=8 and k=2. then it means you have an array of [1,2] and using that you need to return the ways
-* by which you can get 8 as sum using [1,2] like [1,1,1,1,1,1,1] , [2,2,1,1,1,1] etc...
-* we need to find all such sets.
-* */
+ * https://www.youtube.com/watch?v=yFfv03AE_vA
+ * https://leetcode.com/problems/combination-sum/description/
+ * https://www.geeksforgeeks.org/ways-to-sum-to-n-using-natural-numbers-up-to-k-with-repetitions-allowed/
+ * Medium
+ *
+ * if given Sum=8 and k=2. then it means you have an array of [1,2] and using that you need to return the ways
+ * by which you can get 8 as sum using [1,2] like [1,1,1,1,1,1,1] , [2,2,1,1,1,1] etc...
+ * we need to find all such sets.
+ * */
 public class NSumProblem {
 
     public static void main(String[] args) {
@@ -21,16 +21,17 @@ public class NSumProblem {
     }
 
     public static List<List<Integer>> combinations(){
-        int target = 7;
-        int[] candidates = {2,3,6,7};
+        int target = 8;
+        int[] candidates = {10,1,2,7,6,5};
         List<List<Integer>> results = new ArrayList<>();
         List<Integer> subsetList = new ArrayList<>();
         backTracking(target,0,candidates,results,subsetList);
         return results;
     }
 
+    //Self Practice...
     public static void backTracking(int target, int start, int[] candidates,
-                                            List<List<Integer>> results, List<Integer> subsetList){
+                                    List<List<Integer>> results, List<Integer> subsetList){
 
         if(target <0){
             return;
@@ -43,6 +44,27 @@ public class NSumProblem {
             int newTarget = target - candidates[i];
             backTracking(newTarget,i,candidates,results,subsetList);
             subsetList.remove(subsetList.size()-1);
+        }
+
+    }
+
+
+    public static void backTrack2(int target, int start, int[] candidates,
+                                  List<List<Integer>> results, List<Integer> subList){
+
+        if(target<0){
+            return;
+        }
+
+        if(target ==0){
+            results.add(new ArrayList<>(subList));
+        }
+
+        for(int i = start; i<candidates.length; i++){
+            subList.add(candidates[i]);
+            int newTarget = target - candidates[i];
+            backTrack2(newTarget, i,candidates,results,subList);
+            subList.remove(subList.size() - 1);
         }
 
     }
