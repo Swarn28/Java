5f0be49617527189fd6720ac3e3b2b8d636e5b36
Swarn28 <swarnbedi28@gmail.com>
2025-04-04 18:55:19 +0530

    Adding few changes.

---
+++ src/main/ManmeetCode/loops/Test.java
@@ -0,0 +1,25 @@
+package loops;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Test {
+    public static void main(String[] args) {
+//
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("GFG");
+
+        }
+        //infinite loop
+//            for(; ; ){
+//                System.out.println("GFG");
+//            }
+        //while Loop
+        //int i =0;
+//        while(i<10){
+//            System.out.println("GFG");
+//            i++;
+//        }
+    }
+    //System.out.println(i);
+}
--- src/main/java/designPattern/FactoryPattern/readme.txt
+++ src/main/java/designPattern/FactoryPattern/readme.txt
@@ -1,5 +1,5 @@
 In this we created a Creator class and a Product interface. We have different kinds of Vehicle which the client can book.
-Creator class creates the type pf vehicle demanded by the client and calls the methods of the Product interface respectively.
+Creator class creates the type of vehicle demanded by the client and calls the methods of the Product interface respectively.
 
 What actions you need to perform you add them in the Product interface and create classes of it.
 
